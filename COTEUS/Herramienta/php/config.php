f59720ee0580a260094d80c5c657019bce2063c5
Jeremias Cuello <90732956+Jeremias0901@users.noreply.github.com>
2022-05-14 20:25:22 -0300

    Update config.php

--- COTEUS/Herramienta/php/config.php
+++ COTEUS/Herramienta/php/config.php
@@ -1,7 +1,7 @@
 <?php
     $DB_host = "localhost";
     $DB_user = "root";
-    $DB_password = "jerekpo289";
+    $DB_password = "EzL\_yM9e>LG]*_Z";
     $DB_name = "sisdearchivos_herramientas";
 
     $conexion = mysqli_connect($DB_host, $DB_user, $DB_password, $DB_name);
