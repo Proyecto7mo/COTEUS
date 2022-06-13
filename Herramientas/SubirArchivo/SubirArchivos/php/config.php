57c4c0eb3801ef99b3c8403e6b0eeb83242c1863
Jeremias Cuello <90732956+Jeremias0901@users.noreply.github.com>
2022-06-13 19:45:43 -0300

    Borrar directorio Herramientas.
    Directorio no compositorio del proyecto.

--- Herramientas/SubirArchivo/SubirArchivos/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $DB_host = "localhost";
-    $DB_user = "id17353821_jeremias";
-    $DB_password = "EzL\_yM9e>LG]*_Z";
-    $DB_name = "id17353821_sisdearchivos_herramientas";
-
-    $conexion = mysqli_connect($DB_host, $DB_user, $DB_password, $DB_name);
-?>
--- Herramientas/SubirArchivo/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $DB_host = "localhost";
-    $DB_user = "root";
-    $DB_password = "jerekpo289";
-    $DB_name = "sisdearchivos_herramientas";
-
-    $conexion = mysqli_connect($DB_host, $DB_user, $DB_password, $DB_name);
-?>
