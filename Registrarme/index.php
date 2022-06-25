798a7ed984e75e23139ddf0206ed1492ccb10915
Jesús Zerda <jesuszerda18@gmail.com>
2022-06-25 20:00:09 -0300

    Plantilla de formulas y idioma de login/registro

--- Registrarme/index.php
+++ Registrarme/index.php
@@ -55,35 +55,35 @@
             
             <form action='index.php' method="post">
                 <div class="Columns">
-                    <label for="Name">Name</label><br>
-                    <input type='text' name='name' placeholder='nombre' require><br>
-                    <label for="Mail">Enter Email</label><br>
-                    <input type='email' name='email' placeholder='email' require>
+                    <label for="Name">Nombre</label><br>
+                    <input type='text' name='name' placeholder='Nombre' require><br>
+                    <label for="Mail">Ingrese Email</label><br>
+                    <input type='email' name='email' placeholder='Email' require>
                     
                     <!--Capcha-->
                     <label >Aqui va el captcha</label>
                     <br><br><br><br><br><br>
-                    <a href="../Login" style="color: black;">Do you have an account?</a><br>
+                    <a href="../Login" style="color: black;">¿Tienes una cuenta?</a><br>
                 </div>
                 <div class="Columns">
-                    <label for="Last-Name">Last Name</label><br>
-                    <input type='text' name='surname' placeholder='apellido' require><br>
-                    <label for="Username">Username</label><br>
-                    <input type='text' name='nameuser' placeholder='nombre de usuario' require>
+                    <label for="Last-Name">Apellido</label><br>
+                    <input type='text' name='surname' placeholder='Apellido' require><br>
+                    <label for="Username">Nombre de Usuario</label><br>
+                    <input type='text' name='nameuser' placeholder='Usuario' require>
                     
                     <br><br><br><br>
 
                     <input type="submit" value="Sign in">
                 </div>
                 <div class="Columns">
-                    <label for="Birth">Date of Birth</label><br>
+                    <label for="Birth">Fecha de Nacimiento</label><br>
                     <input type="date" style="color: rgb(53, 50, 50);"><br>
-                    <label for="Password">Password</label><br>
+                    <label for="Password">Contraseña</label><br>
                     <input type='password' name='password' require>
-                    <label for="confirm_password">Repita Password</label>
+                    <label for="confirm_password">Repita Contraseña</label>
                     <input type='confirm_password' name='confirm_password' require>
                     <label for="number">Telefono</label>
-                    <input type='number' name='telephono' placeholder='celular' require>
+                    <input type='number' name='telephono' placeholder='Celular' require>
                 </div>
             </form>
         </div>
