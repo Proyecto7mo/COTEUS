47336186c1055b8321e4036b17019df4b7bc50b1
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-08-11 22:23:46 -0300

    archivos movidos a nueva ubicacion correspondiente

---
+++ partials/HTML/nav/nav.php
@@ -0,0 +1,44 @@
+<nav
+      class="navbar navbar-expand-lg navbar-light d-flex justify-content-center"
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand me-5 p-4" href="../Home">COTEUS</a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link btn btn-outline-info rounded-pill shadow-sm mb bg-body rounded"
+                aria-current="page"
+                href="../Home/"
+                >HOME</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../Grupo/">GRUPO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../Gantt/">GANTT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../Cuenta/">CUENTA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../partials/logout.php">SALIR</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
