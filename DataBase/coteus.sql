08e6c6a1a40122bf245033e51f1eba65aa4b3cf9
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-09-17 16:47:39 -0300

    actulizacion de base de datos campo unico de nameuser, email, cuil, telefono y eliminacion de campos innecesarios (claves foraneos sin funcionar)

--- DataBase/coteus.sql
+++ DataBase/coteus.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 07-09-2022 a las 00:39:51
--- Versión del servidor: 8.0.17
--- Versión de PHP: 7.3.10
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-09-2022 a las 21:44:26
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,136 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `calculator_t`
---
-
-CREATE TABLE `calculator_t` (
-  `id_calculator` int(2) NOT NULL,
-  `name_function` varchar(50) NOT NULL,
-  `formula_function` varchar(100) NOT NULL
-) ENGINE=InnoDB;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `chores_t`
---
-
-CREATE TABLE `chores_t` (
-  `id_chores` int(2) NOT NULL,
-  `title` varchar(80) NOT NULL,
-  `assignment` varchar(20) NOT NULL,
-  `duracion` datetime NOT NULL,
-  `startDate` date NOT NULL,
-  `endDate` date NOT NULL,
-  `predecessor` int(2) NOT NULL
-) ENGINE=InnoDB;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `employees_t`
---
-
-CREATE TABLE `employees_t` (
-  `id_user` int(2) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `surname` varchar(20) NOT NULL,
-  `nameuser` varchar(20) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `telephono` varchar(30) NOT NULL,
-  `cuil` int(11) NOT NULL,
-  `id_chores` int(2) NOT NULL,
-  `id_groups` int(2) NOT NULL,
-  `id_files` int(2) NOT NULL,
-  `id_calculator` int(2) NOT NULL,
-  `id_enterprise` int(2) NOT NULL
-) ENGINE=InnoDB;
-
---
--- Volcado de datos para la tabla `employees_t`
---
-
-INSERT INTO `employees_t` (`id_user`, `name`, `surname`, `nameuser`, `password`, `email`, `telephono`, `cuil`, `id_chores`, `id_groups`, `id_files`, `id_calculator`, `id_enterprise`) VALUES
-(31, 'a', 'a', 'a', 'a', 'a@a', '', 0, 0, 0, 0, 0, 0),
-(56, 'b', 'b', 'b', '$2y$10$25icbrgKgganqTPfsyIe7eDbKfqn0Bv3NcLXlq4f0lDeSt4E4EoaC', 'b@b', '0123456789', 0, 0, 0, 0, 0, 0),
-(57, 'jesus', 'zerda', 'jesus9898', '$2y$10$gQU3jC8hdimDFbMcBFTXq.KUORY5AjDM82d7uYvmVo0ieu1PLTH7C', 'jesus@jesus.com', '0123456789', 0, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `enterprise_t`
---
-
-CREATE TABLE `enterprise_t` (
-  `id_enterprise` int(2) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `cuit` int(11) NOT NULL
-) ENGINE=InnoDB;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `files_t`
---
-
-CREATE TABLE `files_t` (
-  `id_files` int(2) NOT NULL,
-  `name` varchar(40) NOT NULL,
-  `path` varchar(50) NOT NULL,
-  `owner` varchar(20) NOT NULL,
-  `lastModification` datetime NOT NULL,
-  `state` tinyint(1) NOT NULL
-) ENGINE=InnoDB;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `groups_t`
 --
 
 CREATE TABLE `groups_t` (
   `id_groups` int(2) NOT NULL,
+  `admin` int(2) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `admin` varchar(20) NOT NULL,
+  `fecha` datetime NOT NULL,
   `id_files` int(2) NOT NULL,
   `id_chores` int(2) NOT NULL
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `groups_t`
+--
+
+INSERT INTO `groups_t` (`id_groups`, `admin`, `name`, `fecha`, `id_files`, `id_chores`) VALUES
+(30, 58, 'a', '2022-09-11 03:28:31', 0, 0),
+(31, 58, 'b', '2022-09-11 03:28:35', 0, 0),
+(32, 59, 'grupo1', '2022-09-11 03:40:47', 0, 0),
+(33, 59, 'grupo2', '2022-09-11 03:41:09', 0, 0);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `calculator_t`
---
-ALTER TABLE `calculator_t`
-  ADD PRIMARY KEY (`id_calculator`);
-
---
--- Indices de la tabla `chores_t`
---
-ALTER TABLE `chores_t`
-  ADD PRIMARY KEY (`id_chores`);
-
---
--- Indices de la tabla `employees_t`
---
-ALTER TABLE `employees_t`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- Indices de la tabla `enterprise_t`
---
-ALTER TABLE `enterprise_t`
-  ADD PRIMARY KEY (`id_enterprise`);
-
---
--- Indices de la tabla `files_t`
---
-ALTER TABLE `files_t`
-  ADD PRIMARY KEY (`id_files`);
 
 --
 -- Indices de la tabla `groups_t`
@@ -167,40 +61,10 @@ ALTER TABLE `groups_t`
 --
 
 --
--- AUTO_INCREMENT de la tabla `calculator_t`
---
-ALTER TABLE `calculator_t`
-  MODIFY `id_calculator` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `chores_t`
---
-ALTER TABLE `chores_t`
-  MODIFY `id_chores` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `employees_t`
---
-ALTER TABLE `employees_t`
-  MODIFY `id_user` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
-
---
--- AUTO_INCREMENT de la tabla `enterprise_t`
---
-ALTER TABLE `enterprise_t`
-  MODIFY `id_enterprise` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `files_t`
---
-ALTER TABLE `files_t`
-  MODIFY `id_files` int(2) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `groups_t`
 --
 ALTER TABLE `groups_t`
-  MODIFY `id_groups` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_groups` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
