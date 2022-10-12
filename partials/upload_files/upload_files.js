99f0d9e53ff106f857b228cb51c5b250b93b2105
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-12 01:52:25 -0300

    mejora 2

--- partials/upload_files/upload_files.js
+++ partials/upload_files/upload_files.js
@@ -29,7 +29,7 @@ input.addEventListener("change", () => {
 upload_files.addEventListener("dragover", (e) => {
   e.preventDefault();
   upload_files.classList.add("dragleave_active");
-  title.textContent = "Suelta el archivo para subirlo.";
+  title.innerText = "Suelta el archivo para subirlo.";
 });
 
 // cuando entra arrastrando
@@ -41,10 +41,8 @@ upload_files.addEventListener("dragleave", (e) => {
  // cuando suelta
 upload_files.addEventListener("drop", (e) => {
   e.preventDefault();
-  files = e.dataTransfer.files;
-  input.files = files;
-  // input.files = e.dataTransfer.files;
-  ShowFiles(files);
+  input.files = (e.dataTransfer.files);
+  ShowFiles(input.files);
   RemoverClaseActive();
 });
 
@@ -65,66 +63,26 @@ function ShowFiles(files){
 
 function processFile(file){
   
-  console.log(file.type);
-  
   const docType = file.type;
   const validExtensions = ['image/jpeg','image/jpg','image/png','image/pdf', 'application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/msword', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.slideshow'];
   
   if(validExtensions.includes(docType)){
-    const fileReader = new FileReader();
-    const id = `file-${Math.random().toString(32).substring(7)}`;
 
-    // handler de FileReader "load", se dispara cuando se carga un archivo a la pagina
-    fileReader.addEventListener('load', (e) => {
-      const fileURL = fileReader.res;
-      const image = `
+    const id = `file-${Math.random().toString(32).substring(7)}`;
+    const card = `
       <div id="${id}" class="preview__file">
-        <img src="${fileURL}" width="50">
         <div class="status">
           <span>${file.name}</span>
-          <span class="preview__fileLoad">
-            Cargando...
-          </span>
+          <span class="preview__fileLoad"> Cargado. </span>
         </div>
       </div>
       `;
       
-      // cargando al DOM las imagenes junto con el nombre de los archivos
-      const preview = document.querySelector('.upload-files-container__preview').innerHTML;
-      document.querySelector('.upload-files-container__preview').innerHTML = image + preview;
-    });
-
-    fileReader.readAsDataURL(file);
-    
-    SubirArchivo(file, id)
+    const preview = document.querySelector('.upload-files-container__preview').innerHTML;
+    document.querySelector('.upload-files-container__preview').innerHTML = card + preview;
   
   }else{
     ArchivoInvalido(file);
-  }
-}
-
-send.addEventListener("click", e => {
-  SubirArchivo (file, id);
-});
-
-async function SubirArchivo (file, id) {
-  const formData = new FormData();
-
-  formData.append("file", file);
-
-  try{
-    const response = await fetch("http://localhost/COTEUS/partials/subirArchivos.php", {
-      method: "POST",
-      body: formData,
-    });
-
-    const responseText = await response.text();
-    console.log(responseText);
-    
-    document.querySelector(`#${id} .preview__fileLoad`).innerHTML = `<span class="preview__fileUploaded"> Archivo Subido Correctamente... </span>`;
-  } catch(error) {
-    const responseText = await response.text();
-    document.querySelector(`#${id} .preview__fileLoad`).innerHTML = `<span class="preview__fileFailed"> Error al subir el archivo... </span>`; 
   }
 }
 
