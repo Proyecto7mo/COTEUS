4c13d67c739421b0bc72e7208a15e27e793c19a2
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-11-11 02:04:11 -0300

    actualizando

--- class/files.php
+++ class/files.php
@@ -1,16 +1,26 @@
 <?php
 
 class files{
-  public $id_files;
+  public $id_file;
   public $name;
-  public $lastmodification;
+  public $last_modification;
 
-  public static function get_record($file) {
-    require "../datos/datos.php";
-    $file_record = false;
+  public function __construct($file_record){
+    $this->id_file = $file_record['id_file'];
+    $this->name = $file_record['name'];
+    $this->last_modification = $file_record['last_modification'];
+  }
+  
+  public function get_groups() {
+    require_once "../datos/datos.php";
+    $group_names = [];
+    
+    $group_names_records = datos::get_groups_of_file($this);
 
-    $file_record = datos::get_file($file);
+    foreach ($group_names_records as $key => $group_name_record) {
+      $group_names[$key] = $group_name_record['group_name'];
+    }
 
-    return $file;
+    return $group_names;
   }
 }
