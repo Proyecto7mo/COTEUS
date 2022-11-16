af4914bf0270813b91537416966a6a3859e3da31
Dracean1257 <crisescorpiodraco55@gmail.com>
2022-11-16 15:02:05 -0300

    Continuando merge

--- class/employee.php
+++ class/employee.php
@@ -8,26 +8,46 @@ class employee{
   public $email;
   public $username;
   public $password;
-  public $telephono;
+  public $telephone;
   public $cuil;
 
-  public function __construct($name = "n/n", $surname = "n/n", $username = "n/n", $email = "nn@nn.com", $password = "n/n", $telephono="00-0000-0000", $cuil = 0){
+  public function __construct($name = "n/n", $surname = "n/n", $username = "n/n", $email = "nn@nn.com", $password = "n/n", $telephono = null, $cuil = null){
     $this->name = $name;
     $this->surname = $surname;
     $this->username = $username;
     $this->email = $email;
     $this->password = $password;
-    $this->telephono=$telephono;
+    $this->telephone = $telephono;
     $this->cuil = $cuil;
+  }
+
+  public static function record_to_object($employee_record){
+    $employee = new employee();
+    
+    foreach ($employee_record as $key => $value) {
+      $employee->$key = $employee_record[$key];
+    }
+    
+    return $employee;
   }
   
   public function get($value, $key = 'id_employee'){
     require_once "../datos/datos.php";
     
     $employee = datos::get_employee($key, $value);
-
+    
     $result = ($employee) ? $employee : null;
+    
+    return $result;
+  }
 
+  public static function get_by_id($id){
+    require_once "../datos/datos.php";
+    
+    $employee = datos::get_employee('id_employee', $id);
+    
+    $result = ($employee) ? $employee : null;
+    
     return $result;
   }
 
@@ -37,24 +57,28 @@ class employee{
     $result = 0;
 
     $result = (datos::insert_employee($this) > 0) ? 1 : -1;
-
+    
     return $result;
   }
 
   public static function login($email, $password){
     require_once "../datos/datos.php";
-
+    
     $employee_record = datos::get_employee('email', $email); // devuelve false si no lo encuentra
 
     if($employee_record && password_verify($password, $employee_record['password'])){
       return $employee_record;
     }
-
+    
     return false;
+
   }
 
-  public function uploadfile(){
-    
+  public function uploadfile($file_name){
+    require_once "../datos/datos.php";
+
+    datos::insert_file($this, $file_name);
+
   }
 
   public static function joingroup($user_id, $groups_id){
@@ -62,35 +86,24 @@ class employee{
   }
 
   public function to_string(){
-    return "<br>" . 
-      "Nombre: " . $this->name . "<br>" . 
-      "Apellido: " . $this->surname . "<br>" . 
-      "Email: " . $this->email . "<br>" . 
-      "NameEmployee: " . $this->username . "<br>" .
-      "Password: " . $this->password . "<br>" .
-      "Cuil: " . $this->cuil . "<br>";
+    return
+      "<p>" . 
+        "ID: " . $this->id_employee . "<br>" . 
+        "Nombre: " . $this->name . "<br>" . 
+        "Apellido: " . $this->surname . "<br>" . 
+        "Email: " . $this->email . "<br>" . 
+        "NameEmployee: " . $this->username . "<br>" .
+        "Password: " . $this->password . "<br>" .
+        "Cuil: " . $this->cuil . "<br>" . 
+      "</p>";
   }
-
-  public static function view_files($employee_folder){
-    $lista = null;
-    $directory = "../files_users/" . $employee_folder;
+  
+  public function get_files(){
     
-    $directory_handler = opendir($directory);
+    require_once "../datos/datos.php";
 
-    return $directory_handler;
-   
-    while($item = readdir($directory_handler)){
-      if($item != "." && $item != ".."){
-        if(is_dir($directory . $item)){
-          $lista .= "<li>Carpeta <a href='https://localhost/COTEUS/files_users/$employee_folder/$item' target='_blank'>$item</a></li>";
-        }
-        else{
-          $lista .= "<li>Archivo <a href='https://localhost/COTEUS/files_users/$employee_folder/$item' target='_blank'>$item</a></li>";
-        }
-      }
-    }
+    $files = datos::get_files_employee($this);
 
-    //return $lista;
-    
+    return $files;
   }
 }
--- class/files.php
+++ class/files.php
@@ -1,18 +1,26 @@
 <?php
 
 class files{
-  public $id_files;
+  public $id_file;
   public $name;
-  public $lastmodification;
+  public $last_modification;
 
-  public static function get_record($file) {
-    require "../datos/datos.php";
-    $file_record = false;
+  public function __construct($file_record){
+    $this->id_file = $file_record['id_file'];
+    $this->name = $file_record['name'];
+    $this->last_modification = $file_record['last_modification'];
+  }
 
-    $file_record = datos::get_file($file);
+  public function get_groups() {
+    require_once "../datos/datos.php";
+    $group_names = [];
 
-    return $file;
+    $group_names_records = datos::get_groups_of_file($this);
+    
+    foreach ($group_names_records as $key => $group_name_record) {
+      $group_names[$key] = $group_name_record['group_name'];
+    }
+
+    return $group_names;
   }
 }
-
-?>
